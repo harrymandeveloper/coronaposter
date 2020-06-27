95a39e2f1231d9a131dc7d3c232a3f2f67aa1b48
Harry Man <harryman@Harrys-MacBook-Air-3.local>
2020-06-27 10:44:02 +0100

    Adding additional options to the app.js and more form construction

--- js/app.js
+++ js/app.js
@@ -22,31 +22,39 @@
     "Make a morning margarita",
     "Press face to pub window",
     "Raise Tigers",
+    "Open wine",
   ];
 
   const secondInstruction = [
     "Consider life choices",
+    "Pour wine",
+    "Facetime with a friend overseas",
     "Remove last loo roll from <br /> hiding place",
     "Search for flour everywhere",
     "Binge watch Dawson's Creek",
     "Fall over backwards",
     "Talk to the animals",
     "Zoom Call Your Mum",
+    "Create anti-gravity boots",
     "Lose the remote down the back of the sofa",
     "Have a nice cold pint",
+    "Open six packets of crisps. Have all of the crisps.",
     "Have a cup of tea",
     "Accidentally hit unmute while farting during a pub quiz",
     "Discover <br /> mysterious island",
     "Assemble Robot Army",
+    "Have a biscuit",
     "Trash talk <br /> Carole Baskin",
     "Tell Child \'That\'s Amazing\'",
     "Chair a meeting in your pyjamas",
     "Find flux capacitor",
     "Find sports almanac",
+    "Complete 1000 piece puzzle of obscure painting of gondoliers",
     "Wash your hands",
   ];
 
   const thirdInstruction = [
+    "Amaze your friends",
     "Steal a tank",
     "Smell the roses",
     "Celebrate",
@@ -58,6 +66,18 @@
     "Drive to Barnard Castle",
     "Return with <br /> unbelievable tales",
   ]
+
+//   button.addEventListener("click", () => {
+
+//     let buttonClick = d.createElement("li");
+
+//     buttonClick.classList.add("list-group-item");
+//     buttonClick.textContent = userText.value;
+    
+//     ul.append(buttonClick);
+    
+// });
+    
 
     billboardText.addEventListener("click", () => {
     let randomNumber = Math.floor(Math.random() * 11);
